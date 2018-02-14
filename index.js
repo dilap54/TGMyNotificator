bef776986b7eca7b505b3f630e8f7b9702026d2c
dilap54 <dilap54@mail.ru>
2018-02-15 02:28:36 +0300

    upgrade version

--- index.js
+++ index.js
@@ -31,58 +31,61 @@ db.set('lastStart', Date.now()).value();
 const bot = new TelegramBot(config.TGtoken, {polling: true});
 
 bot.on('message', function(msg){
+	
 	function sendInstructions(){
 		logger.debug('wrong message', msg.text);
 		bot.sendMessage(msg.chat.id, '/new <название>{0,32} - добавить оповещение\n/all - показать созданные оповещения\n/delete <token> - удалить оповещение');
 	}
+
 	if (msg.entities === undefined || msg.entities[0].type !== 'bot_command'){
 		sendInstructions();
 		return;
 	}
-		var command = msg.text.substr(msg.entities[0].offset, msg.entities[0].length);
-		if (command === '/new'){
-			var name = msg.text.substr(msg.entities[0].length).trim();
-			logger.debug('TM /new', (msg.chat.username || msg.chat.id), name);
-			if (name.length>2 && name.length<32){
-				var newToken = crypto.randomBytes(8).toString('hex');
-				db.get('notificators').push({
-					user: msg.chat.id,
-					name: name,
-					token: newToken
-				}).value();
-				logger.info('TM /new succesful', (msg.chat.username || msg.chat.id), name);
-				bot.sendMessage(msg.chat.id, 'Оповещение добавлено.\nuser: '+msg.chat.id+'\nname: '+name+'\ntoken: '+newToken);
+	
+	var command = msg.text.substr(msg.entities[0].offset, msg.entities[0].length);
+	if (command === '/new'){
+		var name = msg.text.substr(msg.entities[0].length).trim();
+		logger.debug('TM /new', (msg.chat.username || msg.chat.id), name);
+		if (name.length>2 && name.length<32){
+			var newToken = crypto.randomBytes(8).toString('hex');
+			db.get('notificators').push({
+				user: msg.chat.id,
+				name: name,
+				token: newToken
+			}).value();
+			logger.info('TM /new succesful', (msg.chat.username || msg.chat.id), name);
+			bot.sendMessage(msg.chat.id, 'Оповещение добавлено.\nuser: '+msg.chat.id+'\nname: '+name+'\ntoken: '+newToken);
+		}
+		else{
+			logger.debug('TM /new fail', (msg.chat.username || msg.chat.id), msg.text);
+			bot.sendMessage(msg.chat.id, 'Введите /new <Имя>');
+		}
+	}
+	else if (command === '/all'){
+		logger.debug('TM /all', (msg.chat.username || msg.chat.id));
+		var notificators = db.get('notificators').filter({user: msg.chat.id}).value();
+		bot.sendMessage(msg.chat.id, 'Список оповещений:\n'+JSON.stringify(notificators, null, 2));
+	}
+	else if (command === '/delete'){
+		arg = msg.text.substr(msg.entities[0].length).trim();
+		logger.debug('TM /delete', (msg.chat.username || msg.chat.id), arg);
+		if (arg.length >2 && arg.length<32){
+			var deleted = db.get('notificators').remove({user: msg.chat.id, token: arg}).value();
+			if (deleted.length>0){
+				bot.sendMessage(msg.chat.id, '"'+deleted[0].name+'" успешно удален');
 			}
 			else{
-				logger.debug('TM /new fail', (msg.chat.username || msg.chat.id), msg.text);
-				bot.sendMessage(msg.chat.id, 'Введите /new <Имя>');
+				bot.sendMessage(msg.chat.id, 'Ничего не удалено');
 			}
-		}
-		else if (command === '/all'){
-			logger.debug('TM /all', (msg.chat.username || msg.chat.id));
-			var notificators = db.get('notificators').filter({user: msg.chat.id}).value();
-			bot.sendMessage(msg.chat.id, 'Список оповещений:\n'+JSON.stringify(notificators, null, 2));
-		}
-		else if (command === '/delete'){
-			arg = msg.text.substr(msg.entities[0].length).trim();
-			logger.debug('TM /delete', (msg.chat.username || msg.chat.id), arg);
-			if (arg.length >2 && arg.length<32){
-				var deleted = db.get('notificators').remove({user: msg.chat.id, token: arg}).value();
-				if (deleted.length>0){
-					bot.sendMessage(msg.chat.id, '"'+deleted[0].name+'" успешно удален');
-				}
-				else{
-					bot.sendMessage(msg.chat.id, 'Ничего не удалено');
-				}
-				
-			}
-			else{
-				sendInstructions();
-			}
+			
 		}
 		else{
 			sendInstructions();
 		}
+	}
+	else{
+		sendInstructions();
+	}
 });
 
 
